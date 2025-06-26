7f1d7cee49eca37fc3f765703dcb7a4764a54fc8
Evgeniia Tselykh <tea2355@yandex.ru>
2025-06-26 10:03:08 +0300

    Исправлено: "Нужно исправить: теста без шагов быть не должно, тест весь выполнился в предусловии, что не правильно". Теперь весь тест выполняется полностью в самом тесте. После проверки добавления курьера реализуется удаление курьера (только если он был добавлен), что полностью соответствует логике проводимого тестирования.

--- tests/couriers/test_create_courier.py
+++ tests/couriers/test_create_courier.py
@@ -1,13 +1,19 @@
 import allure
+
 from data import *
 from conftests import *
 
 
 class TestCreateCourier:
     @allure.title('Проверка создания курьера')
-    def test_create_courier(self, courier_create):
-        courier_response = courier_create
+    def test_create_courier(self):
+        payload = data.courier_data
+        courier_response = CourierMethods.create_courier(payload)
         assert courier_response[1] == 201 and courier_response[0]['ok']
+        courier_response_1 = CourierMethods.login_courier(payload)
+        id = courier_response_1[0]['id']
+        CourierMethods.delete_courier(id)
+
 
     @allure.title('Проверка невозможности создания двух одинаковых курьеров')
     def test_no_create_same_courier(self, courier_create):
